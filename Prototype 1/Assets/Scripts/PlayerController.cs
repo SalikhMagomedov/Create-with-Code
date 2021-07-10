5cfdcf65c584437f0c6d5b7f3c5284f75e7ef29c
Salikh Magomedov <salikh.magomedov@icloud.com>
2021-07-10 10:34:16 +0300

    Step into the Driver's Seat

--- Prototype 1/Assets/Scripts/PlayerController.cs
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -2,11 +2,19 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5f;
+    private const float Speed = 20f;
+    private const float TurnSpeed = 25f;
+    
+    private float _horizontalInput;
+    private float _forwardInput;
 
     private void Update()
     {
+        _horizontalInput = Input.GetAxis("Horizontal");
+        _forwardInput = Input.GetAxis("Vertical");
+        
         // Move the vehicle forward
-        transform.Translate(Vector3.forward * (Time.deltaTime * speed));
+        transform.Translate(Vector3.forward * (Time.deltaTime * Speed * _forwardInput));
+        transform.Rotate(Vector3.up, Time.deltaTime * TurnSpeed * _horizontalInput);
     }
 }
