1b5e344675147e8acfda1272a70c64f021c06b01
Salikh Magomedov <salikh.magomedov@icloud.com>
2021-07-10 13:53:05 +0300

    Bonus Features 1 - Share your Work

---
+++ Prototype 1/Assets/Scripts/CarMovement.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class CarMovement : MonoBehaviour
+{
+    private const float Speed = 5f;
+
+    private void Update()
+    {
+        // Move the vehicle forward
+        transform.Translate(Vector3.forward * (Time.deltaTime * Speed));
+    }
+}
--- Prototype 1/Assets/Scripts/PlayerController.cs
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -2,16 +2,36 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public GameObject frontCamera;
+    public GameObject backCamera;
+    [Range(1, 2)] public int playerId = 1;
+    public KeyCode cameraTypeKey = KeyCode.F;
+    
     private const float Speed = 20f;
     private const float TurnSpeed = 25f;
     
     private float _horizontalInput;
     private float _forwardInput;
 
+    private string _horizontal;
+    private string _vertical;
+
+    private void Start()
+    {
+        _horizontal = $"Horizontal{playerId}";
+        _vertical = $"Vertical{playerId}";
+    }
+
     private void Update()
     {
-        _horizontalInput = Input.GetAxis("Horizontal");
-        _forwardInput = Input.GetAxis("Vertical");
+        _horizontalInput = Input.GetAxis(_horizontal);
+        _forwardInput = Input.GetAxis(_vertical);
+
+        if (Input.GetKeyDown(cameraTypeKey))
+        {
+            backCamera.SetActive(!backCamera.activeSelf);
+            frontCamera.SetActive(!frontCamera.activeSelf);
+        }
         
         // Move the vehicle forward
         transform.Translate(Vector3.forward * (Time.deltaTime * Speed * _forwardInput));
