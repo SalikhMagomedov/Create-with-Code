8e539371c9b9ed6c0c28ba71f0d0327cbd3c5637
Salikh Magomedov <salikh.magomedov@icloud.com>
2021-07-10 11:01:15 +0300

    Challenge 1 - Plane Programming

---
+++ Prototype 1/Assets/Challenge 1/Scripts/RotatorX.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Challenge_1.Scripts
+{
+    public class RotatorX : MonoBehaviour
+    {
+        public float turnSpeed;
+
+        private void Update()
+        {
+            transform.Rotate(Vector3.forward, turnSpeed * Time.deltaTime);
+        }
+    }
+}
