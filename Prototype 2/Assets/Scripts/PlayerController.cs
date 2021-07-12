9b11bc749d6538b6e406bff1b27adc757071801d
Salikh Magomedov <salikh.magomedov@icloud.com>
2021-07-12 13:53:35 +0300

    Lesson 2.1 - Player Positioning

---
+++ Prototype 2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed = 10f;
+    public float xRange = 10;
+
+    private void Update()
+    {
+        var position = transform.position;
+        if (Mathf.Abs(position.x) > xRange)
+        {
+            transform.position = new Vector3(Mathf.Sign(position.x) * xRange, position.y, position.z);
+        }
+        
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * (horizontalInput * speed * Time.deltaTime));
+    }
+}
