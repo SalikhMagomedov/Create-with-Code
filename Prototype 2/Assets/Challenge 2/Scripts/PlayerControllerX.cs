6915cbe7fd0da89e225a85dbaf1ad697dff98624
Salikh Magomedov <salikh.magomedov@icloud.com>
2021-10-26 11:05:12 +0300

    Challenge 2 - Play Fetch

---
+++ Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+    private bool _isDogSpawned;
+    
+    private const float DogSpawnTimeInterval = .5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && !_isDogSpawned)
+        {
+            _isDogSpawned = true;
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            Invoke(nameof(AllowDogSpawn), DogSpawnTimeInterval);
+        }
+    }
+
+    private void AllowDogSpawn()
+    {
+        _isDogSpawned = false;
+    }
+}
