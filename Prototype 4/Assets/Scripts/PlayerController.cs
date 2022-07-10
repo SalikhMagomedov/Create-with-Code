a6136ed8d190ed269e2033456fba8796cf354aa8
Salikh Magomedov <salikh.magomedov@icloud.com>
2022-07-10 11:22:25 +0300

    Lesson 4.1 - Watch Where You’re Going

---
+++ Prototype 4/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed = 5.0f;
+
+    private Rigidbody _rb;
+    private GameObject _focalPoint;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    private void Start()
+    {
+        _focalPoint = GameObject.Find("Focal Point");
+    }
+
+    private void Update()
+    {
+        var forwardInput = Input.GetAxis("Vertical");
+        _rb.AddForce(_focalPoint.transform.forward * (speed * forwardInput));
+    }
+}
