b16de58618418e869a49c962f475b4cea5856408
Salikh Magomedov <salikh.magomedov@icloud.com>
2022-07-13 23:02:36 +0300

    Lesson 4.2 - Follow the Player

---
+++ Prototype 4/Assets/Scripts/Enemy.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private float speed = 3.0f;
+
+    private Rigidbody _rb;
+    private GameObject _player;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    private void Start()
+    {
+        _player = GameObject.Find("Player");
+    }
+
+    private void Update()
+    {
+        var lookDirection = (_player.transform.position - transform.position).normalized;
+        _rb.AddForce(lookDirection * speed);
+    }
+}
---
+++ Prototype 4/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField] private GameObject enemyPrefab;
+
+    private const float SpawnRange = 9.0f;
+
+    private void Start()
+    {
+        Instantiate(enemyPrefab, GenerateSpawnPosition(), Quaternion.identity);
+    }
+
+    private static Vector3 GenerateSpawnPosition()
+    {
+        var spawnPositionX = Random.Range(-SpawnRange, SpawnRange);
+        var spawnPositionZ = Random.Range(-SpawnRange, SpawnRange);
+
+        return new Vector3(spawnPositionX, 0, spawnPositionZ);
+    }
+}
