3cd2217510b2039f2a6ffd46ca82b5d1186206c3
Salikh Magomedov <salikh.magomedov@icloud.com>
2022-07-28 16:19:14 +0300

    Lesson 4.3 - PowerUp and CountDown

--- Prototype 4/Assets/Scripts/PlayerController.cs
+++ Prototype 4/Assets/Scripts/PlayerController.cs
@@ -1,11 +1,16 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed = 5.0f;
+    [SerializeField] private bool hasPowerUp;
+    [SerializeField] private GameObject powerUpIndicator;
 
     private Rigidbody _rb;
     private GameObject _focalPoint;
+
+    private const float PowerUpStrength = 15.0f;
 
     private void Awake()
     {
@@ -21,5 +26,35 @@ public class PlayerController : MonoBehaviour
     {
         var forwardInput = Input.GetAxis("Vertical");
         _rb.AddForce(_focalPoint.transform.forward * (speed * forwardInput));
+        powerUpIndicator.transform.position = transform.position + new Vector3(0, -.5f, 0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("PowerUp")) return;
+
+        hasPowerUp = true;
+        Destroy(other.gameObject);
+        StartCoroutine(PowerUpCountdownRoutine());
+        powerUpIndicator.SetActive(true);
+    }
+
+    private IEnumerator PowerUpCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+
+        hasPowerUp = false;
+        powerUpIndicator.SetActive(false);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!collision.gameObject.CompareTag("Enemy") || !hasPowerUp) return;
+
+        var enemyRb = collision.gameObject.GetComponent<Rigidbody>();
+        var awayFromPlayer = (collision.gameObject.transform.position - transform.position);
+
+        Debug.Log($"Player collided with {collision.gameObject.name} with powerUp set to {hasPowerUp}");
+        enemyRb.AddForce(awayFromPlayer * PowerUpStrength, ForceMode.Impulse);
     }
 }
